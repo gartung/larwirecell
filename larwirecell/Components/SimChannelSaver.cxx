f0769da3d7c86390611e60f8a68641ae033788a4
Wesley Ketchum <wketchum@fnal.gov>
2018-08-29 10:09:23 -0500

    new simchannel saver component

---
+++ larwirecell/Components/SimChannelSaver.cxx
@@ -0,0 +1,89 @@
+
+#include "SimChannelSaver.h"
+//#include "art/Framework/Principal/Handle.h" 
+
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Core/EDProducer.h"
+
+#include "WireCellIface/IDepo.h"
+#include "WireCellUtil/NamedFactory.h"
+
+#include <algorithm>
+
+WIRECELL_FACTORY(wclsSimChannelSaver, wcls::SimChannelSaver,
+		 wcls::IArtEventVisitor, WireCell::IDepoFilter)
+
+
+using namespace wcls;
+using namespace WireCell;
+
+SimChannelSaver::SimChannelSaver()
+    : received_eos(false)
+{
+}
+
+SimChannelSaver::~SimChannelSaver()
+{
+}
+
+
+WireCell::Configuration SimChannelSaver::default_configuration() const
+{
+    Configuration cfg;
+    return cfg;
+}
+
+void SimChannelSaver::configure(const WireCell::Configuration& cfg)
+{
+}
+
+void SimChannelSaver::produces(art::EDProducer* prod)
+{
+    assert(prod);
+}
+
+void SimChannelSaver::create_simchannels(art::Event &,WireCell::IDepo::vector const&)
+{
+}
+
+void SimChannelSaver::visit(art::Event & event)
+{
+    if(!received_eos){
+	std::cout << "SCSaver: visiting event without receiving EOS."
+		  << " DepVec size is " << in_depo_vec.size()
+		  << std::endl;
+	return;
+    }
+    
+    if(in_depo_vec.size()==0){
+	std::cout << "SCSaver: visiting event but DepoVec size is " << in_depo_vec.size()
+		  << std::endl;
+    }
+
+    create_simchannels(event,in_depo_vec);
+
+    in_depo_vec.clear();
+    received_eos = false;
+}
+
+bool SimChannelSaver::operator()(const WireCell::IDepo::pointer& indepo,
+				 WireCell::IDepo::pointer& outdepo)
+{
+    //collect the depos
+    outdepo = indepo;
+    
+    if(indepo){
+	in_depo_vec.push_back(indepo);
+	received_eos = false;
+    }
+    else{
+	received_eos = true;
+    }
+
+    return true;
+}
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// End:
---
+++ larwirecell/Components/SimChannelSaver.h
@@ -0,0 +1,46 @@
+/** A WCT component which passes through IDepos untouched and saves
+ * their content as a vector of SimChannels
+ * 
+ * Aug29, 2018: Wes started this following guidelines from Brett and Jyoit
+*/
+
+#ifndef LARWIRECELL_COMPONENTS_SIMCHANNELSAVER
+#define LARWIRECELL_COMPONENTS_SIMCHANNELSAVER
+
+#include "WireCellIface/IDepoFilter.h"
+#include "WireCellIface/IConfigurable.h"
+#include "larwirecell/Interfaces/IArtEventVisitor.h"
+
+#include <string>
+#include <vector>
+#include <map>
+
+namespace wcls {
+    class SimChannelSaver : public IArtEventVisitor, 
+                            public WireCell::IDepoFilter,
+                            public WireCell::IConfigurable {
+    public:
+        SimChannelSaver();
+        virtual ~SimChannelSaver();
+
+        /// IArtEventVisitor 
+        virtual void produces(art::EDProducer* prod);
+        virtual void visit(art::Event & event);
+
+        /// IDepoFilter
+        virtual bool operator()(const WireCell::IDepo::pointer& indepo,
+				WireCell::IDepo::pointer& outdepo);
+
+        /// IConfigurable
+        virtual WireCell::Configuration default_configuration() const;
+        virtual void configure(const WireCell::Configuration& config);
+
+    private:
+        WireCell::IDepo::vector in_depo_vec;
+	bool received_eos;
+
+	void create_simchannels(art::Event & event,WireCell::IDepo::vector const&);
+    };
+}
+
+#endif
