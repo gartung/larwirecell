7fc421dbcff98ce662fd466832e92308be211a08
Brooke Russell <brussell@uboonegpvm05.fnal.gov>
2018-09-06 08:32:02 -0500

    need hookup to detectorclocksservice and make parameters fhicl configurable

--- larwirecell/Components/SimChannelSink.cxx
+++ larwirecell/Components/SimChannelSink.cxx
@@ -38,9 +38,9 @@ WireCell::Configuration SimChannelSink::default_configuration() const
     cfg["rng"] = "Random";
     cfg["start_time"] = 0.0*units::ns;
     cfg["readout_time"] = 5.0*units::ms;
-    cfg["tick"] = 0.5*units::ms;
+    cfg["tick"] = 0.5*units::us;
     cfg["nsigma"] = 3.0; 
-    cfg["drift_speed"] = 1.0*units::mm/units::us;
+    cfg["drift_speed"] = 1.1*units::mm/units::us;
     return cfg;
 }
 
@@ -73,97 +73,72 @@ void SimChannelSink::produces(art::EDProducer* prod)
 }
 
 void SimChannelSink::save_as_simchannel(const WireCell::IDepo::pointer& depo){
-  /*
-  art::ServiceHandle<detinfo::DetectorClocksServiceStandard> tss;
-  auto const* ts = tss->provider();
-*/
+
+  //art::ServiceHandle<detinfo::DetectorClocksServiceStandard> tss;
+  //auto const* ts = tss->provider();
+    
+    if(!depo) return;
+
     for(auto face : m_anode->faces()){
-
         auto boundbox = face->sensitive();
+	if(!boundbox.inside(depo->pos())) continue;
 
-	if(boundbox.inside(depo->pos())){
+	for(auto plane : face->planes()){
+	  const Pimpos* pimpos = plane->pimpos();
+	  Binning tbins(m_readout_time/m_tick, m_start_time, m_start_time+m_readout_time);
+	  Gen::BinnedDiffusion bindiff(*pimpos, tbins, m_nsigma, m_rng);
+	  bindiff.add(depo,depo->extent_long() / m_drift_speed, depo->extent_tran());
+	  double depo_dist = pimpos->distance(depo->pos());
+	  std::pair<int,int> wire_pair = pimpos->closest(depo_dist); 
+	  auto& wires = plane->wires();
+	  
+	  unsigned int depo_chan = (unsigned int)wires[wire_pair.first]->channel();
+	  auto channelData = m_mapSC.find(depo_chan);
+	  sim::SimChannel& sc = (channelData == m_mapSC.end())
+	    ? (m_mapSC[depo_chan]=sim::SimChannel(depo_chan))
+	    : channelData->second;
 
-   	    int iplane = -1;
-	    for(auto plane : face->planes()){
-	        ++iplane;
-	  		      
-	        const Pimpos* pimpos = plane->pimpos();
-
-		Binning tbins(m_readout_time/m_tick, m_start_time, m_start_time+m_readout_time);
-
-		Gen::BinnedDiffusion bindiff(*pimpos, tbins, m_nsigma, m_rng);
-		bindiff.add(depo,depo->extent_long() / m_drift_speed, depo->extent_tran());
-
-		double depo_dist = pimpos->distance(depo->pos());
-
-		std::pair<int,int> wire_pair = pimpos->closest(depo_dist); 
-
-		auto& wires = plane->wires();
-
-		//for(auto w=wire_pair.first; w<=wire_pair.second; w++){
-		auto w=wire_pair.first;
-
-		    unsigned int depo_chan = (unsigned int)wires[w]->channel();
-		    std::cout<<"channel "<< depo_chan<<std::endl;
-			    
-		    auto channelData = m_mapSC.find(depo_chan);
-
-		    sim::SimChannel& sc = (channelData == m_mapSC.end())
-		      ? (m_mapSC[depo_chan]=sim::SimChannel(depo_chan))
-		      : channelData->second;
-
-		    double xyz[3] = {depo->pos().x()/units::cm,
-				     depo->pos().y()/units::cm,
-				     depo->pos().z()/units::cm}; 
-		
-		    double energy = 200.0; // !!!!! TEMPORARY !!!!!
-
-		    int id = -10000;
-		    if(depo->prior()){
-		      id = depo->prior()->id();
-		    }
-		    else{
-		      id = depo->id();
-		    }
-		    
-		    //auto temp_time = depo->time()/units::ns;
-		    //auto tdc = ts->TPCG4Time2TDC(temp_time);
-		    
-		    unsigned int temp_time = 100; //depo->time()/units::ns;
-		    /*		
-		    const auto region_binning = pimpos->region_binning();
-		    const auto impact_binning = pimpos->impact_binning();
-		    const double wire_pos = region_binning.center(w);
-
-		    const std::pair<double,double> pitch_range = bindiff.pitch_range(m_nsigma);
-		    const int min_impact = impact_binning.edge_index(wire_pos-0.5*pitch_range.first);
-		    const int max_impact = impact_binning.edge_index(wire_pos+0.5*pitch_range.second);
-
-		    for(int i=min_impact; i<=max_impact; i++){
-		        auto impact_data = bindiff.impact_data(i);
-		        if(!impact_data) continue;
+	  double xyz[3] = {depo->pos().x()/units::cm,
+			   depo->pos().y()/units::cm,
+			   depo->pos().z()/units::cm}; 
+	  double energy = 200.0; // !!!!! TEMPORARY !!!!!
+	  int id = -10000;
+	  if(depo->prior()){ id = depo->prior()->id(); }
+	  else{ id = depo->id(); }
+	
+	  unsigned int temp_time = (unsigned int) (depo->time()/units::us-(-4.05e3)-4050+1.6e3) / (m_tick/units::us); // hacked G4 to tick
+	  //unsigned int temp_time = (unsigned int) (depo->time()/units::us-(-4.05e3) / (m_tick/units::us)); // hacked G4 to TDC
+	  //unsigned int temp_time = (unsigned int) depo->time()/units::us;
+	  //double temp_time = depo->time()/units::us;
+	  //auto tdc = ts->TPCG4Time2TDC(temp_time);
+	  
+	  const std::pair<int,int> impact_range = bindiff.impact_bin_range(m_nsigma);
+	  for(int i=impact_range.first; i<=impact_range.second; i++){
+	    auto impact_data = bindiff.impact_data(i);
+	    if(!impact_data) continue;
 		      
-		        auto wave = impact_data->waveform();
-		        const std::pair<double,double> time_span = impact_data->span(m_nsigma);
-		        const int min_tick = tbins.bin(time_span.first);
-		        const int max_tick = tbins.bin(time_span.second);
-		        for(int t=min_tick; t<=max_tick; t++){
-			    const double tdc = tbins.center(t);
-			    //std::cout<<"tdc "<<tdc<<std::endl;
-			    std::cout<<"charge from wave: "<<wave[t]<<std::endl;
-			    */
-			    double temp_charge = depo->charge();		    
-			    
-			    sc.AddIonizationElectrons(id, 
-						      temp_time, //tdc,
-						      temp_charge, //-wave[t],
-						      xyz, 
-						      energy);			
-			    //}
-			    //}
-			    //} // w
-	    } // plane
-	}    // within bounded box
+	    auto wave = impact_data->waveform();
+	    const std::pair<double,double> time_span = impact_data->span(m_nsigma);
+	    const int min_tick = tbins.bin(time_span.first);
+	    const int max_tick = tbins.bin(time_span.second);
+	    for(int t=min_tick; t<=max_tick; t++){
+	      //const double tdc = tbins.center(t);
+	      //std::cout<<"tdc "<<tdc<<std::endl;
+	      //std::cout<<"charge from wave: "<<wave[t]<<std::endl;
+
+	      //double temp_charge = depo->charge();		    
+	      double temp_charge = wave[t];
+	      if(temp_charge>1){
+		sc.AddIonizationElectrons(id, 
+					  temp_time,			
+					  temp_charge, //-wave[t],
+					  xyz, 
+					  energy);			
+	      }
+
+	    } // t
+	  } // i
+	} // plane
     } //face
       
 }
